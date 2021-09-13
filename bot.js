bce73881fc12f58235af9ee5211afee12fb1c834
Michael Van Kirk <vankirk.6@wright.edu>
2021-09-13 12:03:14 -0400

    removed unnecessary comments

--- bot.js
+++ bot.js
@@ -18,8 +18,7 @@ bot.on('ready', function (evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
+
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
@@ -33,7 +32,6 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     message: 'Pong!'
                 });
             break;
-            // Just add any case commands if you want to..
         }
     }
 });
